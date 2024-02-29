bab4d0f4e5de215e74a38fbedf852250d8f1cb08
amber officil <amberofficial380@gmail.com>
2024-02-29 15:18:21 +0500

    first assignment

---
+++ main.js
@@ -0,0 +1,29 @@
+//  Typesscript Data Types Example1
+//let personName : string = "Ahmed";
+//console.log(personName +"karachi"+ "!");
+//let fullName : string = "Amber";
+//console.log(fullName);
+// Example 2 
+//let message : number = 1000;
+//console.log(message);
+//let Age : number = 25;
+//console.log(Age);
+//console.log(100,200,30000,50000);
+//  Example 3 
+//let isFollow: boolean = true;
+//console.log(isFollow);
+//let hasStarted : boolean = false;
+//console.log(hasStarted);
+//console.log(2===2);
+// Example 4 
+//let a;
+//console.log(a);
+// Example 5 
+//let y =null;
+//console.log(y);
+// Example 6 BigInt
+var x = Number.MAX_SAFE_INTEGER;
+console.log("x =", x);
+// Example 7
+//let s1= Symbol("d1");
+//console.warn(s1);
